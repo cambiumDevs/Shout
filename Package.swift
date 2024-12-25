cf83130e0056587ed2ed7429d53719edd0d95d34
cambiumDevs <apple_devs@cambium.co.il>
2024-12-25 15:37:19 +0200

    Update Package.swift
    changed version of Libssh2Prebuild

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Socket", url: "https://github.com/IBM-Swift/BlueSocket", from: "1.0.46"),
-        .package(name: "CSSH", url: "https://github.com/glushchenko/Libssh2Prebuild.git", from: "1.10.0")
+        .package(name: "CSSH", url: "https://github.com/glushchenko/Libssh2Prebuild.git", from: "1.11.0-OpenSSL-1-1-1u")
     ],
     targets: [
         .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
